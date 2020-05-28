f71346c562390fb6feb60a2221fb4e88a84d7502
xDeaa <andrea_christina@hotmail.fr>
2020-05-28 11:37:06 +0200

    implement G,H

--- src/ex06.challengeaccepted.ts
+++ src/ex06.challengeaccepted.ts
@@ -1,4 +1,4 @@
-function F(b: number, c: number, d: number): number {return (b & c) && (~b & d)}
-
-
-function I(b: number, c: number, d: number) : number {return c ^(b | ~d)}
+const F = (b: number, c: number, d: number): number => (b & c) && (~b & d)
+const G = (b: number, c: number, d: number): number => (b & d) | (c & ~d)
+const H = (b: number, c: number, d: number): number => b ^ c ^ d
+const I = (b: number, c: number, d: number) : number => c ^(b | ~d)
