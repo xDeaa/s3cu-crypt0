d47cc646bf7a8be08c8f549e7a08540d7b6801e8
xDeaa <andrea_christina@hotmail.fr>
2020-05-28 11:38:54 +0200

    change function f

--- src/ex06.challengeaccepted.ts
+++ src/ex06.challengeaccepted.ts
@@ -1,4 +1,4 @@
-const F = (b: number, c: number, d: number): number => (b & c) && (~b & d)
+const F = (b: number, c: number, d: number): number => (b & c) | (~b & d)
 const G = (b: number, c: number, d: number): number => (b & d) | (c & ~d)
 const H = (b: number, c: number, d: number): number => b ^ c ^ d
-const I = (b: number, c: number, d: number) : number => c ^(b | ~d)
+const I = (b: number, c: number, d: number): number => c ^ (b | ~d)
