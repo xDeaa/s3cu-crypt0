d5a8df0f08aa1436ba68cdd2480aa05788e29cd5
xDeaa <andrea_christina@hotmail.fr>
2020-05-28 10:35:09 +0200

    recup jc code

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import convertHexToBase64 from './ex01.hex2base64'
 import encrypt from './ex02.fixedxor'
+import singleByteXOR from './ex03.singlebytexor'
 
 console.log(convertHexToBase64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"));
 console.log(encrypt('1c0111001f010100061a024b53535009181c', '686974207468652062756c6c277320657965'))
+console.log(singleByteXOR('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'))
+
 
