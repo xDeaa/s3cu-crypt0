14c87103f5e640c3add9de5ff5ff75b57567762e
Dylan DE SOUSA <dds1991@hotmail.fr>
2020-05-28 10:37:15 +0200

    Adding export default for ex02

--- src/ex02.fixedxor.ts
+++ src/ex02.fixedxor.ts
@@ -9,4 +9,4 @@ function encrypt(a: string, b: string): string {
   return resultBuffer.toString('hex')
 }
 
-console.log(encrypt('1c0111001f010100061a024b53535009181c', '686974207468652062756c6c277320657965'))
+export default encrypt
